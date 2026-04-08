d444a82af07200b2bcb1c84b5844e8dd0fe40715
Saket goswami <110333651+saketgoswami09@users.noreply.github.com>
2026-04-09 00:58:21 +0530

    Refactor main.jsx to include StrictMode wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from '../src/store/store.js';import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store.js';
+import App from './App.jsx';
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-)
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
