5cf91f6c4fa35a32313215b4d0aaf6d60fd342d6
Saket goswami <110333651+saketgoswami09@users.noreply.github.com>
2026-04-09 00:58:49 +0530

    Refactor App component to use Routes for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-
-import './App.css'
-import Navbar from './components/Navbar'
-import CalendarPage from './Pages/Calendar'
-import Home from './Pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
+import CalendarPage from './Pages/Calendar';
 
 function App() {
-
   return (
     <>
-    <Navbar />
-    <Home />
-    <CalendarPage />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+      </Routes>
     </>
-      
-  )
+  );
 }
 
-export default App
+export default App;
